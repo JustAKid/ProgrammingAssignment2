a1d64229e8b4717ec4a9efae41831ae65bf3a2ef
anonymous <JustAKid@users.noreply.github.com>
2014-07-16 21:07:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix() puts a given matrix into a container which can contain its inverse;
+## cacheSolve() utilizes this "cache" to get a given matrix's inverse faster, if possible.
 
-## Write a short comment describing this function
+## Returns a list containing get/set operations, similar to insturctor's example. 
+## The variable "container" serves as cache here while "x" is a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    container <- NULL
+    set <- function(to_be_set) {
+        x <<- y
+        container <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(mat) container <<- mat
+    getinverse <- function() container
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns a matrix which is the argument's inverse. Attempt to "load" it from the "cache" first.
+## If not exist, then solve it and return.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    result <- x$getinverse()
+    ## If cached
+    if(!is.null(result) {
+        message("Result cached, getting it")
+        return(result)
+    }
+    ## If not cached
+    mat <- x$get()
+    result <- solve(mat)
+    x$setinverse(result)
+    result
 }
